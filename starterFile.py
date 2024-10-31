fd421516f71f1edc70c15eb61bfa3a03be9e6a22
le285materi <le285@kent.ac.uk>
2024-10-31 14:38:48 +0000

    commit

--- starterFile.py
+++ starterFile.py
@@ -2,45 +2,33 @@
 
 import math
 
-def safe_divide(a, b):
-#example with b= 0
- 
-    try:
-        #attempt to perform division
+def safe_divide(a, b): 
+    try: 
         return a/b 
     except ZeroDivisionError: 
-        return #cannot divide by ZERO
+        return "cannot divide by ZERO"
       
+    def process_list (input_list):
+     total= 0
+    for item in input_list:
+        try:
+            num= int(item)
+            total += num**2
+        except (TypeError, ValueError):
 
-#def process_list(input_list):
-#sum of squares
+def nested_operations(a,b):
+    a=int(a)
+    b=int(b)
+    try:
+        division_result=a/b
+        result math.sqrt
+        
+        
 
-#inisalising list
- #test_list= [1,2,3,4,5,6,7,8,9,10]
-
- #print orginal list
- #print("The original list is : " + str(test_list))
-
- # using list comprehension
-# sum of squares
-    # try:
-    #     res = sum([i**2 for i in test_list])
-    #     expect:
-
-
-
-#def nested_operations(a, b):
-
-
-#def process_input():
-
-
+    
+                
+    
+      
 main()
     print(safe_divide(10, 2))
     print(safe_divide(10, 0))
-    # print(process_list([1, '2', 3, 'four', 5]))    
-    # print(nested_operations(16, 4))
-    # print(nested_operations(10, 0))
-    # print(nested_operations('a', 5))
-    # process_input()
-
---
+++ x1.py
@@ -0,0 +1,21 @@
+#def process_list(input_list):
+#sum of squares
+
+#inisalising list
+ #test_list= [1,2,3,4,5,6,7,8,9,10]
+
+ #print orginal list
+ #print("The original list is : " + str(test_list))
+
+ # using list comprehension
+# sum of squares
+    # try:
+    #     res = sum([i**2 for i in test_list])
+    #     expect:
+
+
+
+#def nested_operations(a, b):
+
+
+#def process_input():
